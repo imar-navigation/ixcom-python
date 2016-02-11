b1b2aba4d990fd3a5187ef85256302be549293a7
christianr <christianr@fa3a88b2-825e-5648-bb98-bb0d4e3c2968>
2016-02-11 10:50:59 +0000

    current version
    git-svn-id: http://isvn/svn/rep_IEP-I000261_LINUX-KERNEL/01_Entwicklung/trunk/10_Entwicklung/28_SW_Komponente_XCOM/30_IMPL/XCOM_Client_Python/dependencies/xcom@2422 fa3a88b2-825e-5648-bb98-bb0d4e3c2968

---
+++ build/lib/xcom/__init__.py
@@ -0,0 +1,2 @@
+import xcom.xcomdata
+from .xcomparser import XcomClient
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Setup script for xcom library"""
+
+##############################################################################
+#
+#    Copyright (C) iMAR Navigation GmbH, 2016
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from setuptools import setup
+import os
+import sys
+
+if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 3):
+    sys.exit('Sorry, Python < 3.3 is not supported')
+
+if __name__ == '__main__':
+
+    readmePath = os.path.join(os.path.dirname(__file__), 'README.txt')
+    long_description = open(readmePath, "rt").read()
+
+    setup(name='xcom',
+          version='0.1',
+          description='Library for xcom',
+          author='Christian Reimer',
+          author_email='c.reimer@imar-navigation.de',
+          license='LGPL',
+          keywords=['XCOM'],
+          packages=['xcom'],
+          install_requires=[
+                    'crc16>=0.1.1',
+                ],
+          )
