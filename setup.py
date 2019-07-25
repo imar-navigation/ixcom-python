65ff25558cd7366616c0af8b36a4bb3a1526eb7c
christianr <christianr@fa3a88b2-825e-5648-bb98-bb0d4e3c2968>
2019-07-25 09:09:10 +0000

    v1.0.0
    git-svn-id: http://isvn/svn/rep_IEP-I000261_LINUX-KERNEL/01_Entwicklung/trunk/10_Entwicklung/28_SW_Komponente_XCOM/30_IMPL/XCOM_Client_Python/dependencies/xcom-pkg@11342 fa3a88b2-825e-5648-bb98-bb0d4e3c2968

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python3
 """Setup script for xcom library"""
 
-##############################################################################
-#
-#    Copyright (C) iMAR Navigation GmbH, 2016
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 from setuptools import setup
 import os
 import sys
@@ -32,7 +13,7 @@ if __name__ == '__main__':
     readmePath = os.path.join(os.path.dirname(__file__), 'README.md')
     long_description = open(readmePath, "rt").read()
 
-    setup(name='xcom',
+    setup(name='ixcom',
           version='1.0.0',
           description='Library for communicating with xcom devices over network',
           author='iMAR Navigation GmbH',
