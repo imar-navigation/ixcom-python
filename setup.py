f97f2e0a5a036fe8a2d5b39a42e66e518ee126d6
Maximilian Rehermann <maximilian.rehermann@imar-navigation.de>
2023-05-02 11:27:59 +0200

    update to 1.3.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     long_description = open(readmePath, "rt").read()
 
     setup(name='ixcom',
-          version='1.3.2',
+          version='1.3.3',
           description='Library for communicating with xcom devices over network',
           author='iMAR Navigation GmbH',
           author_email='support@imar-navigation.de',
